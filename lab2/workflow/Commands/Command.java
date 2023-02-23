aaecf5b4ddf58cf73a271bb4a80c062d39dba231
aab137 <a.biryukov2@g.nsu.ru>
2023-02-23 13:36:02 +0700

    Решил, что плохая идея to specify the exceptions которые может бросить метод execute

--- lab2/workflow/Commands/Command.java
+++ lab2/workflow/Commands/Command.java
@@ -1,8 +1,7 @@
 package workflow.Commands;
 
 import workflow.ExecutionContext;
-import workflow.exeption.*;
 
 public interface Command {
-    void execute(ExecutionContext context, String... params) throws StackException, InvalidParameterException, MathException;
+    void execute(ExecutionContext context, String... params) throws Exception;
 }
